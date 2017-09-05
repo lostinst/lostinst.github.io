93d0c072fdaac00dbceb463cd5a9e39c31ce9929
lostinst <lostinst@qq.com>
2017-09-05 12:46:13 +0800

    updated avatar

--- _site/githubupload.sh
+++ _site/githubupload.sh
@@ -1,6 +1,10 @@
 #！ /bin/bash
-
+if [ "$#" = "0" ]
+then
+echo "usage: sh githubupload.sh comments for this update"
+else
 git init
 git add .
-git commit -m "update 45"
+git commit -m "$*"
 git push -u origin master
+fi
